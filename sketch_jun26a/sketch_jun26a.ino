9e60d98f7f36b4d2d9d6d180ecd551b39991520a
Claudio Perez <111537394+b0tk@users.noreply.github.com>
2024-07-04 19:03:54 -0400

    Update sketch_jun26a.ino
    cambio del codigo completo

--- sketch_jun26a/sketch_jun26a.ino
+++ sketch_jun26a/sketch_jun26a.ino
@@ -1,104 +1,48 @@
-#include <BleGamepad.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-// Define los pines de los botones
-const int button1 = 12; // Botón del mástil 1
-const int button2 = 13; // Botón del mástil 2
-const int button3 = 14; // Botón del mástil 3
-const int button4 = 27; // Botón del mástil 4
-const int button5 = 26; // Botón del mástil 5
-const int startButton = 25; // Botón de Start
-const int powerButton = 33; // Botón de Power
-const int strumUpButton = 32; // Botón de Strum Up
-const int strumDownButton = 35; // Botón de Strum Down
+const char* ssid = "TU_SSID";
+const char* password = "TU_PASSWORD";
 
-// Instancia del gamepad BLE
-BleGamepad bleGamepad("Guitarra Botk", "botk", 100);
+const char* serverName = "http://TU_IP_DEL_SERVIDOR:3000/data";
 
 void setup() {
-  // Inicia la emulación del gamepad BLE
-  bleGamepad.begin();
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
 
-  // Configura los pines de los botones como entradas
-  pinMode(button1, INPUT_PULLUP);
-  pinMode(button2, INPUT_PULLUP);
-  pinMode(button3, INPUT_PULLUP);
-  pinMode(button4, INPUT_PULLUP);
-  pinMode(button5, INPUT_PULLUP);
-  pinMode(startButton, INPUT_PULLUP);
-  pinMode(powerButton, INPUT_PULLUP);
-  pinMode(strumUpButton, INPUT_PULLUP);
-  pinMode(strumDownButton, INPUT_PULLUP);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando a WiFi...");
+  }
+  
+  Serial.println("Conectado a la red WiFi");
 }
 
 void loop() {
-  if (bleGamepad.isConnected()) {
-    // Lee el estado de los botones y envía las señales correspondientes del gamepad
-    if (digitalRead(button1) == LOW) {
-      bleGamepad.press(BUTTON_1);
-    } else {
-      bleGamepad.release(BUTTON_1);
-    }
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
 
-    if (digitalRead(button2) == LOW) {
-      bleGamepad.press(BUTTON_2);
-    } else {
-      bleGamepad.release(BUTTON_2);
-    }
+    int32_t rssi = WiFi.RSSI();
+    String json = "{\"rssi\":" + String(rssi) + "}";
 
-    if (digitalRead(button3) == LOW) {
-      bleGamepad.press(BUTTON_3);
-    } else {
-      bleGamepad.release(BUTTON_3);
-    }
+    int httpResponseCode = http.POST(json);
 
-    if (digitalRead(button4) == LOW) {
-      bleGamepad.press(BUTTON_4);
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
     } else {
-      bleGamepad.release(BUTTON_4);
+      Serial.print("Error en la petición: ");
+      Serial.println(httpResponseCode);
     }
-
-    if (digitalRead(button5) == LOW) {
-      bleGamepad.press(BUTTON_5);
-    } else {
-      bleGamepad.release(BUTTON_5);
-    }
-
-    if (digitalRead(startButton) == LOW) {
-      bleGamepad.press(BUTTON_6); // Mapea este botón a BUTTON_6
-    } else {
-      bleGamepad.release(BUTTON_6);
-    }
-
-    if (digitalRead(powerButton) == LOW) {
-      bleGamepad.press(BUTTON_7); // Mapea este botón a BUTTON_7
-    } else {
-      bleGamepad.release(BUTTON_7);
-    }
-
-    if (digitalRead(strumUpButton) == LOW) {
-      bleGamepad.press(BUTTON_8); // Mapea este botón a BUTTON_8
-    } else {
-      bleGamepad.release(BUTTON_8);
-    }
-
-    if (digitalRead(strumDownButton) == LOW) {
-      bleGamepad.press(BUTTON_9); // Mapea este botón a BUTTON_9
-    } else {
-      bleGamepad.release(BUTTON_9);
-    }
+    
+    http.end();
+  } else {
+    Serial.println("Error en la conexión WiFi");
   }
 
-  // Modo de emparejamiento si el botón de Start se mantiene presionado
-  if (digitalRead(startButton) == LOW) {
-    unsigned long startPressTime = millis();
-    while (digitalRead(startButton) == LOW) {
-      delay(50);
-      if (millis() - startPressTime > 3000) { // Mantener presionado durante 3 segundos
-        bleGamepad.end();
-        delay(1000); // Espera un segundo antes de reiniciar el modo de emparejamiento
-        bleGamepad.begin();
-        break;
-      }
-    }
-  }
+  delay(10000); // Enviar datos cada 10 segundos
 }
+
